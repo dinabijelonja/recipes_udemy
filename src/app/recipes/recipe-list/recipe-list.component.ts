6c7d603dc09355b7dfc248f0113dc0844300b12e
Dina <dina.bijelonja@startify.ba>
2023-03-07 16:33:32 +0100

    scnd commit

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+
+import { Recipe } from '../recipe.model';
 
 @Component({
   selector: 'app-recipe-list',
@@ -6,5 +8,14 @@ import { Component } from '@angular/core';
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent {
+  recipes: Recipe[] = [
+    new Recipe('Test Recipe', 'Test Desc', 'https://delicerecipes.com/wp-content/uploads/2020/07/Marbled-beef-meat-5.jpg')
+  ];
+
+  constructor() {}
+
+  ngOnInit() {
+
+  }
 
 }
